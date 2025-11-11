2ec57d0c6d187399e01d2d2969901b32e1946ac7
waghh87 <harshad.w@crestinfosystems.com>
2025-11-11 11:54:21 +0530

    chore: update dependencies and add initial test for homepage

---
+++ tests/testdemo.spec.ts
@@ -0,0 +1,12 @@
+// ➖ Remove this line
+// import { test, expect } from '@playwright/test';
+// ➕ Add this line
+import { test, expect } from "@chromatic-com/playwright";
+
+// Then use as normal 👇
+test("Homepage", async ({ page }) => {  
+  await page.goto("https://merchdominator.com/pricing");  
+  
+  await expect(page).toHaveTitle("Plans & Pricing");  
+  // ...
+});
